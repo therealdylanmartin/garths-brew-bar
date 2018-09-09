b77c48c560d956f149666175e864566fad006c6d
Dylan <dylanmmartin84@gmail.com>
2018-09-09 15:45:47 -0500

    Wordpress simplified.

--- wordpress/wp-content/themes/sketch/js/portfolio.js
+++
@@ -1,8 +0,0 @@
-( function( $ ) {
-
-	// Layout posts that arrive via infinite scroll
-	$( document.body ).on( 'post-load', function () {
-		$( '.site-main > .hentry' ).appendTo( $( '.portfolio-projects' ) );
-	} );
-
-} )( jQuery );
